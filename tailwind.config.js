57638824cb9bc22536003f79bf1852b9953bc67b
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-04 22:30:53 +0200

    added custom mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mono': ['"Space Mono"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
