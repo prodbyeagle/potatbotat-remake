db42ddfdadd6b04f5ceb80b4656c3b8e4826982e
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-05 19:50:46 +0200

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,27 +22,30 @@ const Navbar: React.FC = () => {
       setMenuOpen(false);
    };
 
-   const handleClickOutside = (event: MouseEvent) => {
-      const target = event.target as Node;
-
-      if (dropdownRef.current && !dropdownRef.current.contains(target)) {
-         closeDropdown();
-      }
-      if (target instanceof HTMLElement && !target.closest('.hamburger')) {
-         closeMenu();
-      }
-   };
 
    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+         const target = event.target as Node;
+         const clickedElement = target as HTMLElement;
+
+         const dropdownClicked = dropdownRef.current?.contains(clickedElement);
+         const hamburgerClicked = clickedElement.closest('.hamburger') !== null;
+         const menuClicked = document.querySelector('.absolute.top-16')?.contains(clickedElement);
+
+         if (!dropdownClicked && !hamburgerClicked && !menuClicked) {
+            closeDropdown();
+            closeMenu();
+         }
+      };
+
       document.addEventListener('mousedown', handleClickOutside);
       return () => {
          document.removeEventListener('mousedown', handleClickOutside);
       };
-      //eslint-disable-next-line
    }, []);
 
    return (
-      <nav className="border border-neutral-600 bg-neutral-800/50 text-white text-md rounded-xl backdrop-blur-xl p-6 shadow-lg mb-8 flex items-center justify-between z-50">
+      <nav className="border border-neutral-600 bg-neutral-800/50 text-white text-md rounded-xl backdrop-blur-xl p-6 shadow-lg mb-4 flex items-center justify-between z-50 relative">
          <NavLink to="/" className="mr-4">
             <img
                src="https://potat.app/tatoExplode.gif"
@@ -51,11 +54,11 @@ const Navbar: React.FC = () => {
             />
          </NavLink>
 
-         <div className="hidden md:flex flex-grow justify-center space-x-4 items-center">
+         <div className="hidden md:flex flex-grow justify-center space-x-2 items-center">
             <NavLink
                to="/commands"
                className={({ isActive }) =>
-                  `py-2 px-4 rounded-md transition duration-100 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
+                  `py-2 px-4 rounded-md transition duration-100 ${isActive ? 'bg-neutral-700/50 text-white' : 'hover:bg-neutral-600'}`
                }
             >
                Commands
@@ -63,56 +66,48 @@ const Navbar: React.FC = () => {
             <NavLink
                to="/leaderboard"
                className={({ isActive }) =>
-                  `py-2 px-4 rounded-md transition duration-300 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
+                  `py-2 px-4 rounded-md transition duration-100 ${isActive ? 'bg-neutral-700/50 text-white' : 'hover:bg-neutral-600'}`
                }
             >
                Leaderboard
             </NavLink>
-            {/* <li className="relative flex items-center">
+            <li className="relative flex items-center">
                <button
                   onClick={toggleDropdown}
-                  className={`py-2 px-4 rounded-md transition duration-300 hover:bg-neutral-600 text-left`}
+                  className={`py-2 px-4 rounded-md transition duration-100 hover:bg-neutral-600 text-center`}
                >
                   Utils
                </button>
                {dropdownOpen && (
-                  <ul ref={dropdownRef} className="absolute left-4 top-10 mt-2 w-48 bg-neutral-700 border border-neutral-600 rounded-md shadow-lg z-50">
+                  <ul
+                     ref={dropdownRef}
+                     className="absolute left-0 text-center top-full mt-2 p-1 w-48 backdrop-blur-md bg-neutral-600/30 border border-neutral-600 rounded-md shadow-lg z-50"
+                  >
                      <li>
                         <NavLink
-                           to="/utils/tool1"
+                           to="/redirect"
                            onClick={closeDropdown}
                            className={({ isActive }) =>
-                              `block py-2 px-4 rounded-md hover:bg-neutral-600 ${isActive ? 'bg-neutral-600 text-yellow-400' : 'text-white'}`
+                              `block py-2 px-4 rounded-md mb-2 transition duration-100 hover:bg-neutral-600  ${isActive ? 'bg-neutral-700 text-yellow-400' : 'text-white'}`
                            }
                         >
-                           Tool 1
+                           URL Shortener
                         </NavLink>
                      </li>
                      <li>
                         <NavLink
-                           to="/utils/tool2"
+                           to="/api/docs"
                            onClick={closeDropdown}
                            className={({ isActive }) =>
-                              `block py-2 px-4 rounded-md hover:bg-neutral-600 ${isActive ? 'bg-neutral-600 text-yellow-400' : 'text-white'}`
+                              `block py-2 px-4 rounded-md transition duration-100 hover:bg-neutral-600 ${isActive ? 'bg-neutral-700 hover:bg-neutral-600 text-yellow-400' : 'text-white'}`
                            }
                         >
-                           Tool 2
-                        </NavLink>
-                     </li>
-                     <li>
-                        <NavLink
-                           to="/utils/tool3"
-                           onClick={closeDropdown}
-                           className={({ isActive }) =>
-                              `block py-2 px-4 rounded-md hover:bg-neutral-600 ${isActive ? 'bg-neutral-600 text-yellow-400' : 'text-white'}`
-                           }
-                        >
-                           Tool 3
+                           API Docs
                         </NavLink>
                      </li>
                   </ul>
                )}
-            </li> */}
+            </li>
          </div>
 
          <div className="md:hidden flex items-center">
@@ -124,14 +119,14 @@ const Navbar: React.FC = () => {
          </div>
 
          {menuOpen && (
-            <div className="absolute top-16 left-0 right-0 bg-neutral-800 rounded-md shadow-lg z-50">
+            <div className="absolute top-24 left-0 right-0 bg-neutral-800/50 border border-neutral-600 backdrop-blur-md rounded-md p-1 shadow-2xl z-50">
                <ul className="flex flex-col space-y-1">
                   <li>
                      <NavLink
                         to="/commands"
                         onClick={closeMenu}
                         className={({ isActive }) =>
-                           `block py-2 px-4 rounded-md transition duration-300 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
+                           `block py-2 px-4 rounded-md transition duration-100 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
                         }
                      >
                         Commands
@@ -142,56 +137,34 @@ const Navbar: React.FC = () => {
                         to="/leaderboard"
                         onClick={closeMenu}
                         className={({ isActive }) =>
-                           `block py-2 px-4 rounded-md transition duration-300 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
+                           `block py-2 px-4 rounded-md transition duration-100 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
                         }
                      >
                         Leaderboard
                      </NavLink>
+
                   </li>
-                  <li className="relative flex items-center">
-                     <button
-                        onClick={toggleDropdown}
-                        className={`block w-full py-2 px-4 rounded-md transition duration-300 hover:bg-neutral-600 text-left`}
+                  <li>
+                     <NavLink
+                        to="/redirect"
+                        onClick={closeMenu}
+                        className={({ isActive }) =>
+                           `block py-2 px-4 rounded-md transition duration-100 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
+                        }
                      >
-                        Utils
-                     </button>
-                     {dropdownOpen && (
-                        <ul ref={dropdownRef} className="mt-2 w-full bg-neutral-700 border border-neutral-600 rounded-md shadow-lg">
-                           <li>
-                              <NavLink
-                                 to="/utils/tool1"
-                                 onClick={closeDropdown}
-                                 className={({ isActive }) =>
-                                    `block py-2 px-4 rounded-md hover:bg-neutral-600 ${isActive ? 'bg-neutral-600 text-yellow-400' : 'text-white'}`
-                                 }
-                              >
-                                 Tool 1
-                              </NavLink>
-                           </li>
-                           <li>
-                              <NavLink
-                                 to="/utils/tool2"
-                                 onClick={closeDropdown}
-                                 className={({ isActive }) =>
-                                    `block py-2 px-4 rounded-md hover:bg-neutral-600 ${isActive ? 'bg-neutral-600 text-yellow-400' : 'text-white'}`
-                                 }
-                              >
-                                 Tool 2
-                              </NavLink>
-                           </li>
-                           <li>
-                              <NavLink
-                                 to="/utils/tool3"
-                                 onClick={closeDropdown}
-                                 className={({ isActive }) =>
-                                    `block py-2 px-4 rounded-md hover:bg-neutral-600 ${isActive ? 'bg-neutral-600 text-yellow-400' : 'text-white'}`
-                                 }
-                              >
-                                 Tool 3
-                              </NavLink>
-                           </li>
-                        </ul>
-                     )}
+                        URL Shortener
+                     </NavLink>
+                  </li>
+                  <li>
+                     <NavLink
+                        to="/api/docs"
+                        onClick={closeMenu}
+                        className={({ isActive }) =>
+                           `block py-2 px-4 rounded-md transition duration-100 ${isActive ? 'bg-neutral-700 text-white' : 'hover:bg-neutral-600'}`
+                        }
+                     >
+                        API Docs
+                     </NavLink>
                   </li>
                </ul>
             </div>
