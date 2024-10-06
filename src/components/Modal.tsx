f20dc591871dd27ea98a54151fc4968efdf50129
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-06 17:04:31 +0200

    tiny fix

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Modal.css'; // Stelle sicher, dass die CSS-Datei importiert wird
+import '../styles/Modal.css';
 
 interface ModalProps {
    isOpen: boolean;
