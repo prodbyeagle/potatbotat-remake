c1d66ce60cb979f9f44e3527703464687d2b296b
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-06 14:57:55 +0200

    NotFound Update

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -11,7 +11,7 @@ const NotFound: React.FC = () => {
          />
          <h1 className="text-6xl font-medium duration-500 transition-all mb-4 hover:font-black hover:tracking-widest hover:text-red-500/80">404</h1>
          <p className="text-2xl mb-8">Oops! The page you're looking for doesn't exist.</p>
-         <Link to="/" className="bg-neutral-600/50 hover:bg-neutral-700/50 backdrop-blur-xl border border-neutral-600 text-white font-bold py-2 px-4 rounded-xl hover:rounded-xl transition-all">
+         <Link to="/" className="bg-neutral-600/50 hover:bg-neutral-700/50 backdrop-blur-xl border border-neutral-600 text-white font-bold py-2 px-4 rounded-lg hover:rounded-xl transition-all">
             Go Back to Home
          </Link>
       </div>
