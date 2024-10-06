8f2169eeb9e8beb56244831e064e4d7c8da4ac03
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-06 14:58:19 +0200

    Added New paints and updated the selector ui

--- src/components/Home/PaintSelector.tsx
+++ src/components/Home/PaintSelector.tsx
@@ -5,6 +5,7 @@ import { PaintSelectorProps } from '../../types/Paint';
 const paints = [
    {
       url: "https://cdn.7tv.app/emote/667c887f387822a16b8f57ed/4x.webp",
+      shadow: "drop-shadow(rgb(165, 57, 254) 0px 0px 0.1px) drop-shadow(rgb(127, 17, 187) 1px 1px 0.1px)",
       name: "TwitchCon 2024 Rotterdam"
    },
    {
@@ -13,6 +14,7 @@ const paints = [
    },
    {
       url: "https://cdn.7tv.app/emote/66cff5ea4d26a84ee93b349a/4x.webp",
+      shadow: "drop-shadow(rgb(0, 0, 0) 0px 0px 1px)",
       name: "Sunny ( ◥◣_◢◤ )"
    },
    {
@@ -21,14 +23,17 @@ const paints = [
    },
    {
       url: "https://cdn.7tv.app/emote/66cc9863c66164d0fc0ca93b/4x.webp",
+      shadow: "drop-shadow(rgb(73, 0, 122) 1px 1px 0.1px)",
       name: "Whisper"
    },
    {
       url: "https://cdn.7tv.app/emote/66c59abafefca9b2cbfa1ca5/4x.webp",
+      shadow: "drop-shadow(rgb(238, 0, 255) 0px 0px 4px)",
       name: "Nammerino"
    },
    {
       url: "https://i.gifer.com/origin/ac/ac2ef163f44f7a3ffb0ab3a99ba4b98c_w200.gif",
+      shadow: "drop-shadow(rgb(242, 0, 6) 0px 0px 1px) drop-shadow(rgb(255, 36, 250) 0px 0px 1px) drop-shadow(rgb(4, 30, 255) 0px 0px 1px) drop-shadow(rgb(6, 193, 255) 0px 0px 1px) drop-shadow(rgb(29, 255, 23) 0px 0px 1px) drop-shadow(rgb(163, 210, 0) 0px 0px 1px) drop-shadow(rgb(255, 145, 21) 0px 0px 1px) drop-shadow(rgb(255, 32, 32) 0px 0px 1px)",
       name: "Brainrot!"
    },
    {
@@ -75,44 +80,49 @@ const paints = [
       url: "https://cdn.7tv.app/emote/66f3e4a11ab6daa41ce1e880/4x.webp",
       shadow: "drop-shadow(rgb(255, 174, 237) 0px 0px 0.1px) drop-shadow(rgb(5, 111, 138) 1px 1px 0.1px)",
       name: "What!"
+   },
+   {
+      gradient: "repeating-radial-gradient(circle, rgb(255, 211, 92) 72%, rgb(255, 136, 0) 87%, rgb(245, 122, 41) 88%, rgb(249, 218, 134) 95%)",
+      shadow: "drop-shadow(rgb(204, 160, 0) 0px 0px 1px) drop-shadow(rgb(178, 75, 6) 0px 0px 3.5px)",
+      name: "Solar Flare"
+   },
+   {
+      gradient: "linear-gradient(90deg, rgb(241, 201, 227) 0%, rgb(241, 201, 227) 20%, rgb(241, 172, 217) 20%, rgb(241, 172, 217) 40%, rgb(242, 143, 207) 40%, rgb(242, 143, 207) 60%, rgb(242, 115, 196) 60%, rgb(241, 115, 195) 80%, rgb(242, 86, 185) 80%, rgb(242, 86, 185) 100%)",
+      shadow: "drop-shadow(rgb(242, 86, 185) 0px 0px 0.5px) drop-shadow(rgb(242, 86, 185) 0px 0px 5px)",
+      name: "Life in Plastic"
    }
 ];
 
 const PaintSelector: React.FC<PaintSelectorProps> = ({ isOpen, onClose, onSelect }) => {
    return (
       <Modal isOpen={isOpen} onClose={onClose}>
-         <h2 className="text-2xl font-semibold text-center text-white mb-6">Choose a Paint</h2>
-         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+         <h2 className="text-3xl font-bold text-center text-white mb-6">Choose a Paint</h2>
+         <div className="transition duration-100 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4 p-2">
             {paints.map((paint, index) => (
                <div
                   key={index}
-                  className={`relative w-96 h-24 cursor-pointer rounded-xl overflow-hidden transition-all transform hover:scale-105 hover:shadow-lg border border-neutral-600 bg-neutral-800 ${paint.shadow ? 'shadow-lg' : ''}`}
+                  className={`relative cursor-pointer rounded-xl transition-all duration-200 hover:shadow-none hover:scale-105 bg-neutral-800 overflow-hidden shadow-lg ${paint.shadow ? 'shadow-lg' : ''}`}
                   onClick={() => onSelect(paint)}
                   title={paint.name}
                   style={{
                      backgroundImage: paint.gradient ? paint.gradient : undefined,
-                     filter: paint.shadow ? paint.shadow : undefined
+                     height: '150px',
+                     filter: paint.shadow ? paint.shadow : undefined,
                   }}
                >
                   {paint.url && (
                      <img
                         src={paint.url}
                         alt={paint.name}
-                        className="absolute inset-0 w-full h-full object-cover"
+                        className="absolute inset-0 w-full h-full object-cover opacity-80 transition-opacity duration-300"
                      />
                   )}
-                  <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white text-sm text-center p-2 truncate">
+                  <div className="absolute bottom-0 left-0 right-0 bg-neutral-600/60 backdrop-blur-xl text-white text-sm text-center p-2 truncate z-10">
                      {paint.name}
                   </div>
                </div>
             ))}
          </div>
-         <button
-            onClick={onClose}
-            className="mt-6 w-full bg-red-600 text-white py-2 rounded-xl transition hover:bg-red-700"
-         >
-            Close
-         </button>
       </Modal>
    );
 };
