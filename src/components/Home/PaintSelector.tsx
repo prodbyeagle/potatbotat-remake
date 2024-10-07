10f72c7b6eef7071676a4f5640689f7047fdba1a
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-07 23:03:06 +0200

    rearranged paints

--- src/components/Home/PaintSelector.tsx
+++ src/components/Home/PaintSelector.tsx
@@ -4,13 +4,64 @@ import { PaintSelectorProps } from '../../types/Paint';
 
 const paints = [
    {
+      gradient: "radial-gradient(circle, rgb(255, 148, 148) 12%, rgb(229, 36, 75) 27%, rgb(255, 163, 163) 53%, rgb(243, 18, 74) 81%, rgb(225, 14, 14) 100%)",
+      shadow: "drop-shadow(rgb(255, 0, 30) 0px 0px 0.1px)",
+      name: "Erm."
+   },
+   {
+      gradient: "radial-gradient(circle, #ffe5b8 10%, #fff 25%, #fe8b8b 35%, #fe9fc8 50%, #ff388e 75%, #ff006f 100%)",
+      shadow: "drop-shadow(#ff007b 0px 0px 0.1px) drop-shadow(#ff007b 0px 0px 4px)",
+      name: "Hibiscus"
+   },
+   {
+      gradient: "radial-gradient(circle, rgb(255, 255, 255) 0%, rgb(0, 191, 255) 25%, rgb(61, 181, 255) 30%, rgb(0, 204, 255) 40%, rgb(56, 179, 255) 50%, rgb(0, 145, 255) 60%, rgb(255, 255, 255) 100%)",
+      shadow: "drop-shadow(rgb(0, 132, 255) 0px 0px 4px) drop-shadow(rgb(0, 63, 122) 0px 0px 0.1px)",
+      name: "Crystalline"
+   },
+   {
+      gradient: "repeating-radial-gradient(circle, rgb(82, 183, 255) 0%, rgb(255, 241, 92) 12%, rgb(254, 124, 236) 24%, rgb(93, 184, 248) 36%)",
+      shadow: "drop-shadow(rgb(254, 124, 236) 0px 0px 0.1px) drop-shadow(rgb(254, 124, 236) 0px 0px 4px)",
+      name: "Tie-Dye"
+   },
+   {
+      gradient: "repeating-radial-gradient(circle, rgb(255, 211, 92) 72%, rgb(255, 136, 0) 87%, rgb(245, 122, 41) 88%, rgb(249, 218, 134) 95%)",
+      shadow: "drop-shadow(rgb(204, 160, 0) 0px 0px 1px) drop-shadow(rgb(178, 75, 6) 0px 0px 3.5px)",
+      name: "Solar Flare"
+   },
+   {
+      gradient: "repeating-linear-gradient(90deg, rgb(255, 196, 87) 0%, rgb(255, 130, 5) 8%, rgb(255, 102, 0) 16%, rgb(255, 136, 0) 24%, rgb(255, 189, 66) 32%)",
+      shadow: "drop-shadow(rgb(255, 130, 5) 2px 2px 2px)",
+      name: "Pumpkin Spice"
+   },
+   {
+      gradient: "linear-gradient(90deg, rgb(255, 164, 182) 0%, rgb(255, 255, 255) 20%, rgb(255, 164, 182) 40%, rgb(255, 255, 255) 60%, rgb(255, 164, 182) 80%, rgb(255, 255, 255) 100%)",
+      shadow: "drop-shadow(rgb(221, 90, 160) 0px 0px 8px)",
+      name: "Fairy Glow"
+   },
+   {
+      gradient: "radial-gradient(circle, rgb(238, 255, 0) 0%, rgb(166, 255, 0) 50%, rgb(93, 195, 9) 100%)",
+      shadow: "drop-shadow(rgb(189, 225, 9) 0px 0px 4px)",
+      name: "Uranium"
+   },
+   {
+      gradient: "linear-gradient(90deg, rgb(241, 201, 227) 0%, rgb(241, 201, 227) 20%, rgb(241, 172, 217) 20%, rgb(241, 172, 217) 40%, rgb(242, 143, 207) 40%, rgb(242, 143, 207) 60%, rgb(242, 115, 196) 60%, rgb(241, 115, 195) 80%, rgb(242, 86, 185) 80%, rgb(242, 86, 185) 100%)",
+      shadow: "drop-shadow(rgb(242, 86, 185) 0px 0px 0.5px) drop-shadow(rgb(242, 86, 185) 0px 0px 5px)",
+      name: "Life in Plastic"
+   },
+   {
+      gradient: "linear-gradient(0deg, rgba(255, 241, 168, 1.000) 25%, rgba(250, 229, 41, 1.000) 35%, rgba(253, 151, 35, 1.000) 55.00000000000001%, rgba(242, 86, 32, 1.000) 70%, rgba(227, 22, 22, 1.000) 100%)",
+      shadow: "drop-shadow(0px 0px 0.1px rgba(221, 34, 81, 1.000)) drop-shadow(0px 0px 4px rgba(227, 22, 22, 1.000))",
+      name: "Hellfire"
+   },
+   {
+      gradient: "linear-gradient(90deg, rgba(255, 165, 226, 1.000) 0%, rgba(114, 143, 237, 1.000) 100%)",
+      shadow: "drop-shadow(0px 0px 0.1px rgba(237, 162, 228, 1.000)) drop-shadow(0px 0px 4px rgba(115, 143, 237, 1.000))",
+      name: "Hysteria"
+   },
+   {
       url: "https://cdn.7tv.app/emote/667c887f387822a16b8f57ed/4x.webp",
       shadow: "drop-shadow(rgb(165, 57, 254) 0px 0px 0.1px) drop-shadow(rgb(127, 17, 187) 1px 1px 0.1px)",
       name: "TwitchCon 2024 Rotterdam"
-   },
-   {
-      url: "https://cdn.7tv.app/emote/66f0a612c48e0757a94aa641/4x.webp",
-      name: "TwitchCon 2022 Rotterdam"
    },
    {
       url: "https://cdn.7tv.app/emote/66cff5ea4d26a84ee93b349a/4x.webp",
@@ -27,60 +78,10 @@ const paints = [
       name: "Whisper"
    },
    {
-      gradient: "radial-gradient(circle, rgb(255, 148, 148) 12%, rgb(229, 36, 75) 27%, rgb(255, 163, 163) 53%, rgb(243, 18, 74) 81%, rgb(225, 14, 14) 100%)",
-      shadow: "drop-shadow(rgb(255, 0, 30) 0px 0px 0.1px)",
-      name: "Erm."
-   },
-   {
-      gradient: "linear-gradient(90deg, rgb(255, 164, 182) 0%, rgb(255, 255, 255) 20%, rgb(255, 164, 182) 40%, rgb(255, 255, 255) 60%, rgb(255, 164, 182) 80%, rgb(255, 255, 255) 100%)",
-      shadow: "drop-shadow(rgb(221, 90, 160) 0px 0px 8px)",
-      name: "Fairy Glow"
-   },
-   {
-      gradient: "radial-gradient(circle, rgb(238, 255, 0) 0%, rgb(166, 255, 0) 50%, rgb(93, 195, 9) 100%)",
-      shadow: "drop-shadow(rgb(189, 225, 9) 0px 0px 4px)",
-      name: "Uranium"
-   },
-   {
-      gradient: "radial-gradient(circle, #ffe5b8 10%, #fff 25%, #fe8b8b 35%, #fe9fc8 50%, #ff388e 75%, #ff006f 100%)",
-      shadow: "drop-shadow(#ff007b 0px 0px 0.1px) drop-shadow(#ff007b 0px 0px 4px)",
-      name: "Hibiscus"
-   },
-   {
-      gradient: "repeating-linear-gradient(90deg, rgb(255, 196, 87) 0%, rgb(255, 130, 5) 8%, rgb(255, 102, 0) 16%, rgb(255, 136, 0) 24%, rgb(255, 189, 66) 32%)",
-      shadow: "drop-shadow(rgb(255, 130, 5) 2px 2px 2px)",
-      name: "Pumpkin Spice"
-   },
-   {
-      gradient: "radial-gradient(circle, rgb(255, 255, 255) 0%, rgb(0, 191, 255) 25%, rgb(61, 181, 255) 30%, rgb(0, 204, 255) 40%, rgb(56, 179, 255) 50%, rgb(0, 145, 255) 60%, rgb(255, 255, 255) 100%)",
-      shadow: "drop-shadow(rgb(0, 132, 255) 0px 0px 4px) drop-shadow(rgb(0, 63, 122) 0px 0px 0.1px)",
-      name: "Crystalline"
-   },
-   {
-      gradient: "repeating-radial-gradient(circle, rgb(82, 183, 255) 0%, rgb(255, 241, 92) 12%, rgb(254, 124, 236) 24%, rgb(93, 184, 248) 36%)",
-      shadow: "drop-shadow(rgb(254, 124, 236) 0px 0px 0.1px) drop-shadow(rgb(254, 124, 236) 0px 0px 4px)",
-      name: "Tie-Dye"
-   },
-   {
-      url: "https://cdn.7tv.app/emote/66f3e4a11ab6daa41ce1e880/4x.webp",
-      shadow: "drop-shadow(rgb(255, 174, 237) 0px 0px 0.1px) drop-shadow(rgb(5, 111, 138) 1px 1px 0.1px)",
-      name: "What!"
-   },
-   {
-      gradient: "repeating-radial-gradient(circle, rgb(255, 211, 92) 72%, rgb(255, 136, 0) 87%, rgb(245, 122, 41) 88%, rgb(249, 218, 134) 95%)",
-      shadow: "drop-shadow(rgb(204, 160, 0) 0px 0px 1px) drop-shadow(rgb(178, 75, 6) 0px 0px 3.5px)",
-      name: "Solar Flare"
-   },
-   {
-      gradient: "linear-gradient(90deg, rgb(241, 201, 227) 0%, rgb(241, 201, 227) 20%, rgb(241, 172, 217) 20%, rgb(241, 172, 217) 40%, rgb(242, 143, 207) 40%, rgb(242, 143, 207) 60%, rgb(242, 115, 196) 60%, rgb(241, 115, 195) 80%, rgb(242, 86, 185) 80%, rgb(242, 86, 185) 100%)",
-      shadow: "drop-shadow(rgb(242, 86, 185) 0px 0px 0.5px) drop-shadow(rgb(242, 86, 185) 0px 0px 5px)",
-      name: "Life in Plastic"
-   },
-   {
       url: "https://cdn.7tv.app/emote/66c5455c981479cde1c36660/4x.webp",
       shadow: "drop-shadow(rgb(236, 37, 80) 0px 0px 0.1px) drop-shadow(rgb(0, 0, 0) 1px 1px 0.1px)",
       name: "Webros"
-   }
+   },
 ];
 
 
